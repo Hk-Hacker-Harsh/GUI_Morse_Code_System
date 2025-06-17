f4271405ad2225c825a842140033d6b81356a985
Hk Hacker <codingwithme7@gmail.com>
2025-06-17 09:30:16 +0530

    Main file created

---
+++ Main.py
@@ -0,0 +1,14 @@
+#Libraries
+import tkinter as tk             #GUI
+import winsound as ws            #Morse Sound
+import pyaudio as audio
+
+
+#Code
+
+
+
+
+#Credit
+# Name : Harsh Khandal
+# Email : codingwithme7@gmail.com
